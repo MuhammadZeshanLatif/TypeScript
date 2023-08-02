d876cb9ce7bb407959a99989f323d5d31745f5c9
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-08-02 10:49:37 -0700

    s
    s

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-// let message: string = "Hello typescript";
-// let firstyName = "Muhammad Zeeshan ";
-// let laastName: string = "Latif";
-// let fullName: string = firstyName + laastName;
-// let sentence: string = "My name is Zeeshan and I am Hafi";
-// let sentence2: string = "My name is Zeeshan and I am Hafiz";
-// let sentenceLength: number = sentence.length;
-// let upperCaseText: string = sentence.toUpperCase();
-// let lowerCaseText: string = sentence.toLowerCase();
-// let longString: string = sentence.slice(0, 11);
-// let product: string = "Badam";
-// let price: number = 10;
-// let display: string = `The ${product} has price is ${price}`;
-// function isEven(a: number) {
-//   return a%4==0 && a%8==0;
-// }
-// class Person{
-//   name:string="";
-//   age:number=0;
-//   greting(){
-//     return "My name is "+this.name+" and i am " +this.age+" years old";
-//   }
-// }
-var usr = { name: "Zeeshan", Id: 123, display: function () { return "Hello"; } };
-console.log(usr);
+// // let message: string = "Hello typescript";
+// // let firstyName = "Muhammad Zeeshan ";
+// // let laastName: string = "Latif";
+// // let fullName: string = firstyName + laastName;
+// // let sentence: string = "My name is Zeeshan and I am Hafi";
+// // let sentence2: string = "My name is Zeeshan and I am Hafiz";
+// // let sentenceLength: number = sentence.length;
+// // let upperCaseText: string = sentence.toUpperCase();
+// // let lowerCaseText: string = sentence.toLowerCase();
+// // let longString: string = sentence.slice(0, 11);
+// // let product: string = "Badam";
+// // let price: number = 10;
+// // let display: string = `The ${product} has price is ${price}`;
+// // function isEven(a: number) {
+// //   return a%4==0 && a%8==0;
+// // }
+// // class Person{
+// //   name:string="";
+// //   age:number=0;
+// //   greting(){
+// //     return "My name is "+this.name+" and i am " +this.age+" years old";
+// //   }
+// // }
+// var usr = { name: "Zeeshan", Id: 123, display: function () { return "Hello"; } };
+// console.log(usr);
+console.dir(window.navigator);
